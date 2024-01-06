d9ca189b1ffcf5077b6e5f8be904e123c8313a32
William Watson <williampwatson1991@gmail.com>
2024-01-06 17:33:28 -0500

    feat: improving the layout

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ export default function App() {
         <TextInput style={styles.textInput} placeholder='Your Course Goal!' />
         <Button title='ADD' />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>Course Goals</Text>
       </View>
     </View>
@@ -18,17 +18,27 @@ export default function App() {
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50
+    paddingTop: 50,
+    paddingHorizontal: 16,
+    flex: 1,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     padding: 8,
     marginRight: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
   });
